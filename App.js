fc40ee9a6cc5c7a095306a8bd6ec5974991ec693
Rilton Franzone <riltonfranzone@gmail.com>
2020-01-26 14:42:23 -0300

    categories list

--- App.js
+++ App.js
@@ -1,35 +1,46 @@
 import React, {useState, useEffect} from 'react'
-import {Text, View, StyleSheet, FlatList, Image, Dimensions, ScrollView, TextInput} from 'react-native'
+import {Text, View, StyleSheet, FlatList, Image, Dimensions, ScrollView, TextInput, TouchableOpacity} from 'react-native'
 import Swiper from 'react-native-swiper'
 import Constants from 'expo-constants'
-
 let {width, height} = Dimensions.get('window')
 
 function App (){
   const[dataBanner, setDataBanner] = useState([])
   const [isLoading, setIsLoading] = useState('')
+  const [dataCategories, setDataCategories] = useState([])
+  const [selectCategory, setSelectCategory] = useState(0)
 
-  function fetchBanner(){
+  function fetchData(){
     const url = 'http://tutofox.com/foodapp/api.json'
     return fetch(url)
     .then(response => response.json())
     .then(jsonResponse => {
       setDataBanner(jsonResponse.banner),
       setIsLoading(false)
+      setDataCategories(jsonResponse.categories)
     })
     .catch(error => console.error(error))
   }
 
+  function renderItem(item){
+    return(
+      <TouchableOpacity style={[styles.itemCategory, {backgroundColor: item.color}]} onPress={() => setSelectCategory(item.id)}>
+        <Image style={styles.imageCategory} resizeMode='contain' source={{uri: item.image}} />
+        <Text style={styles.nameCategory}>{item.name}</Text>
+      </TouchableOpacity>
+    )
+  }
+
   useEffect(() => {
-    fetchBanner()
+    fetchData()
   }, [])
 
   return(
     <ScrollView >
       <View style={styles.container}>
-        <View style={styles.containerLogo}>
+        <View style={styles.containerHeader}>
           <Image style={styles.logo} source={require('./images/foodapp.png')} resizeMode='contain'/>
-          <Swiper style={styles.swiper} autoplay={true} autoplayTimeout={2}>
+          <Swiper style={styles.swiper} autoplay={true} autoplayTimeout={2.5}>
               {
                 dataBanner.map(itemBan =>(
                   <Image style={styles.imageBanner} showsPagination={true} resizeMode='contain'
@@ -39,6 +50,18 @@ function App (){
               }
           </Swiper>
         </View>
+      </View>
+
+      <View style={styles.containerCategories}>
+          <Text style={styles.titleCategories}>Categories</Text>
+          <FlatList horizontal={true} data={dataCategories}
+          renderItem ={({item}) => renderItem(item)} 
+          keyExtractor = {(item, index) => index.toString()}
+          showsHorizontalScrollIndicator={false}
+          />
+          <View style={{height: 20}}>
+
+          </View>
       </View>
     </ScrollView>
   )
@@ -57,7 +80,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#f2f2f2',
   },
-  containerLogo:{
+  containerHeader:{
     alignItems: 'center',
     width: width
   },
@@ -69,5 +92,31 @@ const styles = StyleSheet.create({
     width: width - 40,
     borderRadius: 10,
     marginHorizontal: 20
+  },
+  containerCategories:{
+    width: width,
+    borderRadius: 20,
+    paddingVertical: 20,
+    backgroundColor: '#fff'
+  },
+  titleCategories:{
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 10
+  },
+  itemCategory:{
+    margin: 5, 
+    alignItems: 'center',
+    borderRadius: 10,
+    padding: 10
+  },
+  imageCategory:{
+    width: 100,
+    height: 80
+  },
+  nameCategory:{
+    fontWeight: 'bold',
+    fontSize: 22
   }
 })
